3ceb33ea19271159ef6379f00494a23d56a4516d
Hamid47hrs <hamid47.hrs@gmail.com>
2023-11-03 15:27:35 +0330

    Create article detail.

--- articles/views.py
+++ articles/views.py
@@ -1,13 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from . import models
 
 
 def articles_list(request):
     articles = models.Articles.objects.filter(status="published").order_by(
-        "created_date"
+        "-created_date"
     )
     context = {
         "articles": articles,
     }
 
     return render(request, "articles/articles_list.html", context=context)
+
+
+def articles_detail(request, slug):
+    article = models.Articles.objects.get(slug=slug)
+
+    context = {
+        "article": article,
+    }
+
+    return render(
+        request,
+        "articles/article_detail.html",
+        context=context,
+    )
