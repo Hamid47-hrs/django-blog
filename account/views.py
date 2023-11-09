ba445334652779f0c950f99099aeddc8a7c18aa1
Hamid47hrs <hamid47.hrs@gmail.com>
2023-11-09 16:38:59 +0330

    Create 'login' url, view and template.

--- account/views.py
+++ account/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as user_login
 
 
 def signup(request):
@@ -7,11 +8,27 @@ def signup(request):
         form = UserCreationForm(request.POST)
 
         if form.is_valid():
-            form.save()
+            user = form.save()
+            user_login(request, user)
 
-            return redirect("accounts:login")
+            # return redirect("accounts:login")
 
     else:
         form = UserCreationForm()
 
     return render(request, "signup.html", {"form": form})
+
+
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            user_login(request, user)
+            return redirect("/")
+
+    else:
+        form = AuthenticationForm()
+
+    return render(request, "login.html", {"form": form})
